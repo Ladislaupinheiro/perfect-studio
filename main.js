55eb5e5b6d71808964fb123a882d73bb954e444c
ladislaupinheiro <66391372+Ladislaupinheiro@users.noreply.github.com>
2023-06-29 09:59:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+document.getElementById("event").addEventListener("click",gerarcod)
+
+function gerarcod(){
+
+   let nomecliente = document.getElementById("NC").value
+
+document.getElementById("sect").innerHTML = nomecliente + Math.random().toFixed(6)
+
+
+
+
+
+
+
+
+
+
+}
+
